a1bee6fcff6c48b6a5f602bf1d82f2608910e1f0
Sergiy Fomin <sergiy.fomin@sourcedgroup.com>
2021-06-18 14:30:24 -0400

    fixed the issue with min score not rendering. added a different calc to the function responsible for calculating a winner score

--- src/features/scoreboard/scoreboard.js
+++ src/features/scoreboard/scoreboard.js
@@ -30,6 +30,7 @@ export const Scoreboard = () => {
         scoreBoardResults,
         Score
     );
+    console.log(scoreBoardResults);
 
     const resultArr = ScoreBoardUtils.checkForNewRecord(
         score,
--- src/features/scoreboard/scoreboardUtils.js
+++ src/features/scoreboard/scoreboardUtils.js
@@ -51,6 +51,17 @@ export const getRowElements = (topScores, Score) => {
 }
 
 
+const getCountOfMinNums = (topScoresArr) => {
+    const minNum = Math.min(...topScoresArr);
+    const minNumsCount = topScoresArr.reduce(
+        (accum, currVal) => {
+            const val = currVal === minNum ? 1 : 0;
+            return accum + val;
+        }
+    );
+    return [minNum , minNumsCount];
+}
+
 /**
  * This function is used to determine if new top 10 score is achieved
  * 
@@ -66,11 +77,15 @@ export const checkForNewRecord = (score, topScoresArray, boardLength) => {
     let newRecordSet = false;
     let whichIndex;
 
+    // if there are > 1 min score in results, new records won't show on the board.
+    // therefore I will exclude them from the calculation.
+
     //adding this condition to excludre run for when the board is still loading
     if (topScoresArray === null) {
         return [newRecordSet, whichIndex];
     }
 
+    // if this is the first score on the board
     if (
         topScoresArray.length === 0
         && score !== 0
@@ -79,37 +94,49 @@ export const checkForNewRecord = (score, topScoresArray, boardLength) => {
         return [newRecordSet, whichIndex];
     }
 
-
-    console.log(topScoresArray);
+    // if this is the new highest score on the board
     if (score > topScoresArray[0]) {
-        console.log(score);
-        console.log('hi from best score');
         newRecordSet = true;
         whichIndex = 0;
         return [newRecordSet, whichIndex];
     }
 
-    for (
-        let i = 0;
-        i < topScoresArray.length - 1;
-        i++
-    ) {
-        if (
-            score < topScoresArray[i]
-            && score >= topScoresArray[i + 1]
-        ) {
-            newRecordSet = true;
-            whichIndex = i + 1;
-            break;
-        }
-    }
-
+    // if this is not a new top score, but the board is not full yet
     if (
         !newRecordSet
         && topScoresArray.length < boardLength
         && score !== 0
     ) {
         newRecordSet = true;
+        return [newRecordSet, whichIndex];
+    }
+    
+    if (topScoresArray.length !== 0) {
+        const [minNum, minNumsCount] = getCountOfMinNums(topScoresArray);
+        // for everything else
+        for (
+            let i = 0;
+            i < topScoresArray.length - 1;
+            i++
+        ) {
+            if (
+                score < topScoresArray[i]
+                && score >= topScoresArray[i + 1]
+                && score !== minNum
+            ) {
+                newRecordSet = true;
+                whichIndex = i + 1;
+                break;
+            } 
+            else if  (
+                score === minNum
+                && minNumsCount < 1
+            ) {
+                newRecordSet = true;
+                whichIndex = i + 1;
+                break; 
+            }
+        }
     }
 
     return [newRecordSet, whichIndex];
