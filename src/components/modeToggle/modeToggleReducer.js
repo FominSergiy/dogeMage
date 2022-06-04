998827c0cc5730d86a948b99fca9fb7e15ce3c08
see-hoo <see-hoo@gmail.com>
2022-06-04 15:50:52 -0400

    #18 Add theme reducer

---
+++ src/components/modeToggle/modeToggleReducer.js
@@ -0,0 +1,20 @@
+import THEMES from "../../constants";
+
+const initState = { theme: "light" };
+
+export const switchMode = (state=initState, action) => {
+    switch(action.type) {
+        case "SWITCH_TO_DARK":
+            return {
+                ...state,
+                theme: action.theme
+            }
+        case "SWITCH_TO_LIGHT":
+            return {
+                ...state,
+                theme: action.theme
+            }
+        default:
+            return state;
+    }
+}
