3410c1cafadf7d721dfe014478e9967bdf6f090d
see-hoo <see-hoo@gmail.com>
2022-06-05 08:58:41 -0400

    #18 Add boiler-plate theme toggle

---
+++ src/components/themeToggle/ThemeToggle.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./themeToggle.css";
+// import { THEMES } from "../../constants";
+
+const ThemeToggle = () => {
+    return(
+        <button
+            className="theme-toggle"
+            title="Toggle light and dark"
+        >
+            click me
+        </button>
+    );
+};
+
+export default ThemeToggle;
