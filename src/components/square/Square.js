f14aad0ff5d3e926a59a944fcc9bbaaaa0d6db08
see-hoo <see-hoo@gmail.com>
2022-08-06 16:46:45 -0400

    #17 Add styling for board for small and large screens

--- src/components/square/Square.js
+++ src/components/square/Square.js
@@ -3,17 +3,19 @@ import PropTypes from "prop-types";
 import { getImage } from "./squareUtils";
 import { useSelector } from "react-redux";
 import "./square.css";
+import { getScreenSize } from "../game/gameUtils";
 
 
 const Square = (props) => {
     const theme = useSelector(store => store.themeToggle).theme;
+    const screenSize = getScreenSize();
     const squareObj = props.squareObj;
 
     const mage = getImage("mage", squareObj.mage);
     const coin = getImage("coin", squareObj.coin);
 
     return (
-        <div className={`square ${theme}`} id={props.id} >
+        <div className={`square ${theme} ${screenSize}`} id={props.id} >
             {mage || coin}
         </div>
     );
